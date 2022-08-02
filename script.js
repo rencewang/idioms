8cc91ecdf2b13962293721e6e65fe74f6f99519e
rencewang <47406212+rencewang@users.noreply.github.com>
2022-08-01 17:33:25 -0700

    style changes

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ const idioms = [
   ['西湖月满', '东浙潮来'],
   ['君臣一梦', '今古空名'],
   ['灯华易散', '不再惊奇'],
+  ['饮冰十年', '难凉热血'],
+  ['明月大江', '清风山冈'],
+  ['随心所欲', '随遇而安'],
+  ['一切皆流', '无物常驻'],
+  ['念念不忘', '必有回响'],
+  '沧海桑田',
+  '水到渠成',
   '朱颜辞镜',
   '溪山无尽',
   '人生海海',
@@ -17,7 +24,24 @@ const idioms = [
   '盘尼西林',
   '满载星海',
   '道法自然',
+  '胸有丘壑',
+  '沧海桑田',
+  '水到渠成',
+  '人生海海',
+  '循规蹈矩',
+  '前程似锦',
+  '中正仁和',
   '应该就是现在',
+  '君子之交淡如水',
+  '平平淡淡才是真',
+  '一切都是最好的安排',
+  '想好了就去做',
+  '在盛大的夏天里席卷而过',
+  '一切都和后来不同',
+  '人会来就会离开',
+  '每个人只能陪你走一段路',
+  '热爱可抵岁月蹉跎',
+  '若你困于无风之地',
 ];
 
 // reorder idioms for every load
@@ -45,7 +69,7 @@ const shuffle = (array) => {
 // increase stroke width of the text
 const increaseStroke = (element) => {
   const stroke = element.getAttribute('stroke-width');
-  element.setAttribute('stroke-width', parseFloat(stroke) + 0.05);
+  element.setAttribute('stroke-width', parseFloat(stroke) + 0.2);
 };
 
 const addIdiomsToContainer = (idioms, container) => {
@@ -60,7 +84,7 @@ const addIdiomsToContainer = (idioms, container) => {
 
     div.innerHTML = `
       <svg viewbox="0 0 100 20" xmlns="http://www.w3.org/2000/svg" width="100%" height="200px" preserveAspectRatio="none">
-        <text x="0" y="15" textlength="100%" lengthadjust="spacingAndGlyphs" stroke="green" stroke-width="0">${idiom}</text>
+        <text x="0" y="15" textlength="100%" lengthadjust="spacingAndGlyphs" stroke="#3d5af1" stroke-width="0">${idiom}</text>
       </svg>
     `;
     container.appendChild(div);
@@ -84,4 +108,11 @@ window.onload = function () {
 // scroll backwardContainer in reverse direction
 window.addEventListener('scroll', (e) => {
   backwardContainer.style.bottom = `-${window.scrollY}px`;
+  if (window.scrollY >= forwardContainer.clientHeight - window.innerHeight) {
+    window.scrollTo({ top: 0 });
+  } else if (window.scrollY == 0) {
+    window.scrollTo({
+      top: forwardContainer.clientHeight - window.innerHeight,
+    });
+  }
 });
