158aa9c1fa40269f5913693d26a13d9b9ff4961b
rencewang <47406212+rencewang@users.noreply.github.com>
2022-11-10 16:37:41 -0500

    update content upon click

--- idioms.js
+++ idioms.js
@@ -1,4 +1,4 @@
-const idioms_object = [
+const idioms = [
   {
     text: ['心有定境', '不住因果'],
     interpretation: 'fdsa',
@@ -30,32 +30,288 @@ const idioms_object = [
     source: 'fdsa',
   },
   {
-    text: '心有定境，不住因果',
+    text: ['不以物喜', '不以己悲'],
     interpretation: 'fdsa',
     source: 'fdsa',
   },
   {
-    text: '心有定境，不住因果',
+    text: ['苍山负雪', '明烛天南'],
     interpretation: 'fdsa',
     source: 'fdsa',
   },
   {
-    text: '心有定境，不住因果',
+    text: ['西湖月满', '东浙潮来'],
     interpretation: 'fdsa',
     source: 'fdsa',
   },
   {
-    text: '心有定境，不住因果',
+    text: ['君臣一梦', '今古空名'],
     interpretation: 'fdsa',
     source: 'fdsa',
   },
   {
-    text: '心有定境，不住因果',
+    text: ['灯华易散', '不再惊奇'],
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: ['饮冰十年', '难凉热血'],
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: ['明月大江', '清风山冈'],
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: ['随心所欲', '随遇而安'],
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: ['一切皆流', '无物常驻'],
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: ['念念不忘', '必有回响'],
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: ['虹销雨霁', '彩彻云衢'],
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: ['千手百眼', '天下人间'],
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '沧海桑田',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '水到渠成',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '朱颜辞镜',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '溪山无尽',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '人生海海',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '将暮未暮',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '盘尼西林',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '满载星海',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '道法自然',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '胸有丘壑',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '沧海桑田',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '循规蹈矩',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '前程似锦',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '中正仁和',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '与神同行',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '和光同尘',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '试上超然',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '举世盛名',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '天下无双',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '与谁同坐',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '江流日下',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '私人记忆',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '宏大叙事',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '一来二去',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '急景凋年',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '试上超然',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '倥偬一生',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '金色传说',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '国士无双',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '君子不器',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '尽人事听天命',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '应该就是现在',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '想好了就去做',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '君子之交淡如水',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '平平淡淡才是真',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '人会来就会离开',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '虽千万人吾往矣',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '一切都和后来不同',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '在此世的星空之外',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '热爱可抵岁月蹉跎',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '若你困于无风之地',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '一切都是最好的安排',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '在盛大的夏天里席卷而过',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: '每个人只能陪你走一段路',
+    interpretation: 'fdsa',
+    source: 'fdsa',
+  },
+  {
+    text: ['善变惊惶之人', '雨落狂流之夜'],
     interpretation: 'fdsa',
     source: 'fdsa',
   },
 ];
-
-export function idioms() {
-  return idioms_object;
-}
--- script.js
+++ script.js
@@ -1,69 +1,3 @@
-const idioms = [
-  ['心有定境', '不住因果'],
-  ['饮冰十年', '难凉热血'],
-  ['此心光明', '亦复何言'],
-  ['浮舟沧海', '立马昆仑'],
-  ['年华向晚', '岁月沉香'],
-  ['万古长空', '一朝风月'],
-  ['不以物喜', '不以己悲'],
-  ['苍山负雪', '明烛天南'],
-  ['西湖月满', '东浙潮来'],
-  ['君臣一梦', '今古空名'],
-  ['灯华易散', '不再惊奇'],
-  ['饮冰十年', '难凉热血'],
-  ['明月大江', '清风山冈'],
-  ['随心所欲', '随遇而安'],
-  ['一切皆流', '无物常驻'],
-  ['念念不忘', '必有回响'],
-  ['虹销雨霁', '彩彻云衢'],
-  ['千手百眼', '天下人间'],
-  '沧海桑田',
-  '水到渠成',
-  '朱颜辞镜',
-  '溪山无尽',
-  '人生海海',
-  '将暮未暮',
-  '盘尼西林',
-  '满载星海',
-  '道法自然',
-  '胸有丘壑',
-  '沧海桑田',
-  '水到渠成',
-  '人生海海',
-  '循规蹈矩',
-  '前程似锦',
-  '中正仁和',
-  '与神同行',
-  '和光同尘',
-  '试上超然',
-  '举世盛名',
-  '天下无双',
-  '与谁同坐',
-  '江流日下',
-  '私人记忆',
-  '宏大叙事',
-  '一来二去',
-  '急景凋年',
-  '金色传说',
-  '国士无双',
-  '君子不器',
-  '尽人事听天命',
-  '应该就是现在',
-  '想好了就去做',
-  '君子之交淡如水',
-  '平平淡淡才是真',
-  '人会来就会离开',
-  '虽千万人吾往矣',
-  '一切都和后来不同',
-  '在此世的星空之外',
-  '热爱可抵岁月蹉跎',
-  '若你困于无风之地',
-  '一切都是最好的安排',
-  '在盛大的夏天里席卷而过',
-  '每个人只能陪你走一段路',
-  ['善变惊惶之人', '雨落狂流之夜'],
-];
-
 // reorder idioms for every load
 const shuffle = (array) => {
   let modifiedArray = array.slice();
@@ -87,17 +21,22 @@ const addIdiomsToContainer = (idioms, container) => {
 
     div.onclick = (element) => {
       increaseStroke(element.target);
-      // document.querySelector('#interpreter').style.display = 'block';
 
       document.querySelector('#interpreter').style.visibility = 'visible';
       document.querySelector('#interpreter').style.opacity = '1';
-      document.querySelector('#interpreter-text').innerHTML = idiom;
+
+      document.querySelector('#interpreter-text').innerHTML = idiom.text;
+      document.querySelector('#interpreter-interpretation').innerHTML =
+        idiom.interpretation ? `${idiom.interpretation}` : '';
+      document.querySelector('#interpreter-source').innerHTML = idiom.source
+        ? `${idiom.source}`
+        : '';
     };
 
     div.innerHTML = `
       <svg viewbox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" width="100%" height="200px" preserveAspectRatio="none">
-        <text x="0" y="75" lengthadjust="spacingAndGlyphs" textlength="100%" stroke="#ffd102" stroke-width="0">
-          ${idiom}
+        <text x="0" y="75" lengthadjust="spacingAndGlyphs" textlength="100%" stroke="#000000" stroke-width="0">
+          ${idiom.text}
         </text>
       </svg>
     `;
